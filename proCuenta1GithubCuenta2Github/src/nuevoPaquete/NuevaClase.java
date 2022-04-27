bd8fde19bd822a5087e1031f466bef569ce64f1b
Usuario <Usuario@WIN7_ENT2>
2022-04-27 18:52:11 +0200

    Modificacion de la rama

--- proCuenta1GithubCuenta2Github/src/nuevoPaquete/NuevaClase.java
+++ proCuenta1GithubCuenta2Github/src/nuevoPaquete/NuevaClase.java
@@ -5,6 +5,7 @@ public class NuevaClase {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Esta es una nueva clase");
+		System.out.println("Modificación de la Rama");
 	}
 
 }
