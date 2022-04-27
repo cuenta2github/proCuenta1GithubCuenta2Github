840152def0899ac0bc3577c3175d674fb36a057f
Usuario <Usuario@WIN7_ENT2>
2022-04-27 18:14:49 +0200

    Commit hasta 12

---
+++ proCuenta1GithubCuenta2Github/src/proCuenta1GithubCuenta2Github/HolaMundo.java
@@ -0,0 +1,12 @@
+package proCuenta1GithubCuenta2Github;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.err.println("Hola Mundo");
+		System.out.println("Adios Mundo");
+		System.out.println("Cambio 12");
+	}
+
+}
